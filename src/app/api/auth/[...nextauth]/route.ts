f78e9e514f4185f688ffd2088a9f57adea34d71c
IvaTsu <ivan.tsud@tretton37.com>
2025-02-15 12:51:27 +0100

    feat: config Google OAuth and MongoDB adapter

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,20 @@
+import { env } from "@/env.mjs";
+import client from "@/lib/db";
+import { MongoDBAdapter } from "@auth/mongodb-adapter";
+import GoogleProvider from "next-auth/providers/google";
+import NextAuth, { AuthOptions } from "next-auth";
+
+const authOptions: AuthOptions = {
+  adapter: MongoDBAdapter(client),
+  providers: [
+    GoogleProvider({
+      clientId: env.GOOGLE_CLIENT_ID,
+      clientSecret: env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+  secret: env.AUTH_OPTION_SECRET,
+};
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
